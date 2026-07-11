d33df7ec21b04f9341fd46588b5f3208a4dbd788
hayoung <khy07181@gmail.com>
2026-07-11 10:09:28 +0900

    release: 1.0.0

--- Casks/cloister.rb
+++ Casks/cloister.rb
@@ -1,6 +1,6 @@
 cask "cloister" do
   version "1.0.0"
-  sha256 "REPLACED_PER_RELEASE"
+  sha256 "54936bd7988f066d01c9ee881cd801a88ad962fee6ee1978e3203ac3168527cb"
 
   url "https://github.com/khy07181/homebrew-cloister/releases/download/v#{version}/cloister-#{version}.dmg"
   name "Cloister"
